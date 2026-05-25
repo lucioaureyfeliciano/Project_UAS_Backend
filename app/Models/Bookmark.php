8206aff0459747f33e8783e2883896bcac82edc5
claudialinetta <claudialinetta07@gmail.com>
2026-05-26 02:59:05 +0700

    add models for comment, notification, bookmark with custom IDs

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Bookmark extends Model
+{
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['user_id', 'tweet_id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = 'bookmark_' . Str::random(16);
+            }
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tweet(): BelongsTo
+    {
+        return $this->belongsTo(Tweet::class);
+    }
+}
