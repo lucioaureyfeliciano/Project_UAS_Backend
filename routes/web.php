cf116f41aaaf109a9986349ac57d111c10ccd2ec
claudialinetta <claudialinetta07@gmail.com>
2026-05-27 23:59:02 +0700

    add routes for comments, notifications, and bookmarks

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TweetController;
+use App\Http\Controllers\Web\CommentWebController;
+use App\Http\Controllers\Web\NotificationWebController;
+use App\Http\Controllers\Web\BookmarkWebController;
 
 Route::get('/', function () {
     return redirect('/login');
@@ -29,4 +32,27 @@ Route::middleware('auth')->group(function () {
     Route::get('/tweets', [TweetController::class, 'show_tweets']);
     Route::delete('/tweets/{id}', [TweetController::class, 'delete_tweet']);
     Route::put('/tweets/{id}', [TweetController::class, 'edit_tweet']);
+});
+
+Route::middleware('auth')->group(function () {
+    // comments
+    Route::get('/tweets/{tweet_id}/comments', [CommentWebController::class, 'index'])->name('comments.index');
+    Route::get('/tweets/{tweet_id}/comments/create', [CommentWebController::class, 'create'])->name('comments.create');
+    Route::post('/tweets/{tweet_id}/comments', [CommentWebController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{id}/edit', [CommentWebController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{id}', [CommentWebController::class, 'update'])->name('comments.update');
+    Route::get('/comments/{id}', [CommentWebController::class, 'show'])->name('comments.show');
+    Route::delete('/comments/{id}', [CommentWebController::class, 'destroy'])->name('comments.destroy');
+
+    // notifications
+    Route::get('/notifications', [NotificationWebController::class, 'index'])->name('notifications.index');
+    Route::put('/notifications/{id}/read', [NotificationWebController::class, 'markAsRead'])->name('notifications.markAsRead');
+    Route::delete('/notifications/{id}', [NotificationWebController::class, 'destroy'])->name('notifications.destroy');
+    Route::get('/notifications/{id}', [NotificationWebController::class, 'show'])->name('notifications.show');
+
+    // bookmarks
+    Route::get('/bookmarks', [BookmarkWebController::class, 'index'])->name('bookmarks.index');
+    Route::post('/bookmarks', [BookmarkWebController::class, 'store'])->name('bookmarks.store');
+    Route::delete('/bookmarks/{tweet_id}', [BookmarkWebController::class, 'destroy'])->name('bookmarks.destroy');
+    Route::get('/bookmarks/{id}', [BookmarkWebController::class, 'show'])->name('bookmarks.show');
 });
