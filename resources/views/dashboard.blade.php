7a5dee0437b2d29119ffaa638fa311084f83d5b6
fakhzulraflis <rfakhzul@gmail.com>
2026-05-28 23:17:06 +0700

    fix: double date shown on tweet post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -371,7 +371,6 @@
                                 •
                                 {{ $tweet->created_at->diffForHumans() }}
                             </small>
-                            <small>By {{ $tweet->user?->username ?? 'Unknown' }} · {{ $tweet->created_at->diffForHumans() }}</small>
                         </div>
 
                         {{-- AKSI SISI KANAN TWEET CARD --}}
